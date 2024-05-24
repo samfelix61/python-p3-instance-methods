e139f79f1104d237b52856103cd6a8e210d20002
samfelix61 <samfelix61@gmail.com>
2024-05-24 19:31:37 +0300

    complete

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def __init__(self, name=""):
+        self.name = name
+       
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
